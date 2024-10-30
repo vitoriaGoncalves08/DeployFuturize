b1d5d6117fc7740893cca26f47be9992b4424ffb
Vitória Gonçalves Passos <VitoriaGoncalvesPassos@hotmail.com>
2024-10-29 23:18:20 -0300

    teste

--- src/main/java/med/voll/api/controller/AlocacaoProjetoController.java
+++ src/main/java/med/voll/api/controller/AlocacaoProjetoController.java
@@ -13,7 +13,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
